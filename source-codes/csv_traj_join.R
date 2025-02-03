fa4ab372ecd5557a53373a04911b81db3da94611
Iuri Ferreira <95420024+iep-ferreira@users.noreply.github.com>
2025-02-03 16:10:50 -0300

    changes - excel tables

--- source-codes/csv_traj_join.R
+++ source-codes/csv_traj_join.R
@@ -11,19 +11,28 @@ csv_traj_join <- function(pasta, n) {
   # Selecionar aleatoriamente n arquivos
   arquivos_selecionados <- sample(arquivos, n)
   
+  metadados <- lapply(arquivos_selecionados, function(file) {
+    lines <- readLines(file, n = 2)  # Captura apenas as 2 primeiras linhas
+    unique_id <- sub("unique_id,", "", lines[1])  # Remove o prefixo
+    hash_landscape <- sub("hash_landscape,", "", lines[2])  # Remove o prefixo
+    return(data.frame(unique_id, hash_landscape, stringsAsFactors = FALSE))
+  })
+  metadados_final <- do.call(rbind, metadados)
+  
   # Ler e unir os arquivos em um único data frame
-  data_frames <- lapply(arquivos_selecionados, read.csv)
+  data_frames <- lapply(arquivos_selecionados, function(file) read.csv(file, skip = 2))
   data_final <- do.call(rbind, data_frames)
   
-  return(list(traj_table = data_final, selected_files = arquivos_selecionados))
+  return(list(traj_table = data_final, selected_files = arquivos_selecionados, 
+              selected_files_metadados = metadados_final))
 }
 
 # função para selecionar arquivos e reunir dados de trajetórias
 select_trajectories <- function(dir_path, n_sample){
   sim_res <- csv_traj_join(dir_path, n_sample)
-  sim_res$selected_files
+  #sim_res$selected_files
   traj <- sim_res$traj_table
-  colnames(traj) <- c("id", "step", "week", "x", "y", "landscape_hash")
+  colnames(traj) <- c("id", "step", "week", "x", "y")
   traj$id_numeric <- as.numeric(factor(traj$id))
   return(traj)
 }
--- source-codes/record_trajectories.R
+++ source-codes/record_trajectories.R
@@ -1,17 +1,33 @@
 record_trajectories <- function(mov_obj, hash_value){
   ngroup <- length(mov$trajectory)
-  for(i in 1:ngroup){
+  if(ngroup != 1){stop("Simular apenas um individuo por vez")}
+  #for(i in 1:ngroup){
     id_aux <- UUIDgenerate()
     linhas <- length(mov$time.step)
     trajectory_df <- data.frame(
-      unique_id <- rep(id_aux, linhas),
+      id = rep(id_aux, linhas),
       step = 1:mov$time.step,
       week = as.integer((1:mov$time.step/30240))+1,
-      x = round(mov$trajectory[[i]][, 2], 2),
-      y = round(mov$trajectory[[i]][, 3], 2), 
-      hash_landscape = rep(hash_value, linhas)
+      x = round(mov$trajectory[[1]][, 2], 4),
+      y = round(mov$trajectory[[1]][, 3], 4)
     )
-    file_name <- paste0("./data_sim/", id_aux,".csv")  
-    write.csv(trajectory_df, file_name, row.names = FALSE)  
-  }
+    #colnames(trajectory_df) <- c("step", "week", "x", "y")
+    
+    file_name <- paste0("./data_sim/", id_aux, ".csv")
+    write.csv(NULL, file_name, row.names = FALSE)
+  
+    con <- file(file_name, "w")
+    
+    writeLines(c(
+      paste0("unique_id,", id_aux),
+      paste0("hash_landscape,", hash_value),
+      "id,step,week,x,y"  
+    ), con)
+    
+    close(con)
+    
+    # Escrever os dados da trajet?ria corretamente no formato CSV
+    write.table(trajectory_df, file_name, sep = ",", col.names = FALSE, 
+                row.names = FALSE, append = TRUE, quote = FALSE)
+      
 }
